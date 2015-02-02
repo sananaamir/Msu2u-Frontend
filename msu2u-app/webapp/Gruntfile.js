5b86925a3f6518d61ec1afa897a74bd9abff53ee
Jokii19 <sananaamir@gmail.com>
2015-02-02 14:01:46 -0600

    included font awesome and made some changes to grunt tasks

--- msu2u-app/webapp/Gruntfile.js
+++ msu2u-app/webapp/Gruntfile.js
@@ -319,9 +319,14 @@ module.exports = function (grunt) {
           cwd: '.tmp/images',
           dest: '<%= yeoman.dist %>/images',
           src: ['generated/*']
-        }, {
+        }/*, {
           expand: true,
           cwd: 'bower_components/bootstrap/dist',
+          src: 'fonts/*',
+          dest: '<%= yeoman.dist %>'
+        }*/,{
+          expand: true,
+          cwd: 'bower_components/font-awesome/',
           src: 'fonts/*',
           dest: '<%= yeoman.dist %>'
         }]
